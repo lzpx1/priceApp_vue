a1295ab50909dc19f08c1fb55c447e062d5cb85f
Administrator <Administrator@lzpx1>
2017-06-23 18:07:49 +0800

    日历初始化

--- src/components/PageInit.vue
+++ src/components/PageInit.vue
@@ -24,8 +24,21 @@
 	
 	    pages.params.control = nav;
 	};
-
+	function TimeInit(){
+		var myApp = new Framework7();
+		for(let i = 0; i<arguments.length;i++){
+			let myCalendar = myApp.calendar({
+		   	 	input: arguments[i],
+		   	 	monthNames: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月' , '九月' , '十月', '十一月', '十二月'],
+		   	 	dayNamesShort:['周日', '周一', '周二', '周三', '周四', '周五', '周六'],
+		   	 	onDayClick:function (p, dayContainer, year, month, day){
+		   	 		myCalendar.close();
+		   	 	}
+			});
+		}
+	}
 	export default{
-	   SwiperInit
+	   SwiperInit,
+	   TimeInit
 	}
 </script>
